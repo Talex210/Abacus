dfbacadb0403e1883f65c5a669e75ff37e4b189a
TALEX <tal.89@mail.ru>
2022-06-05 22:55:48 +0300

    Примерная разметка

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import Frame5 from './img/Frame5.png';
+import Vector5 from './img/Vector5.png';
 
 function App() {
-  return (
-    <div className="App">
-      Hello
-    </div>
-  );
+    return (
+        <div className='App'>
+            <img src={Frame5} alt='frame'/>
+            <img src={Frame5} alt='frame'/>
+            <img src={Frame5} alt='frame'/>
+            <img src={Frame5} alt='frame'/>
+            <img src={Frame5} alt='frame'/>
+            <img className='stone' src={Vector5} alt='frame'/>
+        </div>
+    );
 }
 
 export default App;
