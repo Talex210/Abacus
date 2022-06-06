709519c4a9b546c774288b8196b7db0b9aa0b101
TALEX <tal.89@mail.ru>
2022-06-06 16:18:24 +0300

    Создание компонентов. Реализация динамически изменяющейся рамки через useState.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import './App.css';
-import Frame5 from './img/Frame5.png';
 import Vector5 from './img/Vector5.png';
+import {useState} from "react";
+import Frame from "./Components/Frame/Frame";
 
 function App() {
+
+const [lengthFrame, setLength] = useState([1, 2, 3, 4, 5, 6, 7]);
+const [value, setValue] = useState('');
+
+const changeLength = () => {
+    let newLength = [];
+    for (let i = 1; i <= value; i ++) {
+        newLength.push(i);
+    }
+    setLength(newLength);
+    setValue('')
+}
+
     return (
         <div className='App'>
-            <img src={Frame5} alt='frame'/>
-            <img src={Frame5} alt='frame'/>
-            <img src={Frame5} alt='frame'/>
-            <img src={Frame5} alt='frame'/>
-            <img src={Frame5} alt='frame'/>
+            <Frame lengthFrame={lengthFrame}/>
             <img className='stone' src={Vector5} alt='frame'/>
+            <input type='text' placeholder='Set length' value={value} onChange={event => setValue(event.target.value)}/>
+            <button onClick={changeLength}>Change</button>
         </div>
     );
 }
---
+++ src/Components/Frame/Frame.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Frame5 from "../../img/Frame5.png";
+
+const Frame = (props) => {
+    return (
+        <div>
+            {props.lengthFrame.map(f =>
+                <img src={Frame5} alt='frame' key={f}/>
+            )}
+        </div>
+    );
+};
+
+export default Frame;
